3b2dc07875c9d519ac6a556c5c0d97fb54b914a6
Jacksonpickett <jacksonpickett@amdg.rockhursths.edu>
2016-12-13 14:00:43 -0600

    test comments

--- CuppitProject/CuppitTests/CuppitTests.swift
+++ CuppitProject/CuppitTests/CuppitTests.swift
@@ -10,17 +10,17 @@ import XCTest
 @testable import Cuppit
 
 class CuppitTests: XCTestCase {
-    
+    //set up
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
+    //tear down
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
+    //test example
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
