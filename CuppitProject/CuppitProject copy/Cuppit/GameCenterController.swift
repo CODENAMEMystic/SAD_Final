c855e855e5559f883456e19564846865be3b3c85
Tanner1638 <tanner1638@gmail.com>
2016-12-12 22:08:42 -0600

    removed copy
    Huh, why was this here? o-o

--- CuppitProject/CuppitProject copy/Cuppit/GameCenterController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  GameCenterController.swift
-//  Cuppit
-//
-//  Created by Austin Sands on 12/5/16.
-//  Copyright © 2016 Austin Sands. All rights reserved.
-//
-
-import Foundation
--- CuppitProject/CuppitProject copy/Cuppit/PublicGameFunctions.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Public.swift
-//  Cuppit
-//
-//  Created by Austin Sands on 12/5/16.
-//  Copyright © 2016 Austin Sands. All rights reserved.
-//
-
-import Foundation
